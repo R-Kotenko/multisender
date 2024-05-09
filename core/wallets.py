b916c860a92a9540a0c3c9652a6da097603c3360
romak <romakotenko19@gmail.com>
2024-05-09 12:26:03 +0300

    0.1

--- core/wallets.py
+++ core/wallets.py
@@ -16,7 +16,6 @@ def get_address(private_key):
     return address
 
 
-
 def wallets_creator():
 
     mnemo = Mnemonic("english")
@@ -28,44 +27,3 @@ def wallets_creator():
     with open('data/new_wallets.txt', 'a') as file:
         file.write(f'{address}:{private_key}:{seed_phrase}\n')
         log.success(f"{address} | created successfully")
-
-
-
-# def seed():
-#     if exists(path='mnemonic_dop.txt'):
-#         with open(file='mnemonic_dop.txt', mode='r', encoding='utf-8-sig') as file:
-#             seed_list = [row.strip() for row in file]
-#     else:
-#         seed_list = []
-#
-#     log.info(f"Downloaded successfully cookies: {len(seed_list)} in Base64")
-#
-#     try:
-#         for seed in seed_list:
-#
-#             # split_list = dop_data[1:-1].split(', ')
-#             #
-#             # # Видаляємо одинарні лапки з кожного елемента
-#             # new_list = [element.strip("'") for element in split_list]
-#
-#             # print(type(new_list))
-#             # print(dop_data)
-#
-#             # data = ' '.join(new_list)
-#             # print(data)
-#
-#             key = get_private_key(seed)
-#
-#
-#             address = get_address(key)
-#
-#
-#             with open('data_dop.txt', 'a') as file:
-#                 file.write(f"{address}:{key}:{seed}\n")
-#
-#         log.success('Converter successfully in JSON')
-#
-#     except Exception as err:
-#         log.error(err)
-
-
